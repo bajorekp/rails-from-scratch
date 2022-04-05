e6e01e7b30334cd5b25661dc9079e35ecb82a11a
bajorekp <bajorekp@gmail.com>
2022-04-06 00:52:45 +0200

    Multithread TCP server

--- main.rb
+++ main.rb
@@ -5,19 +5,25 @@ socket = TCPServer.new(4242)
 
 begin
   puts "The server is running on port 4242..."
-  loop do
-    client = socket.accept
-    puts "Hi user! I'm waiting for your message"
+  servers = 2.times.map do |pool_id|
+    Thread.new do
+      loop do
+        puts "Thread #{pool_id} is wating for user..."
+        client = socket.accept
+        puts "Hi user! I'm waiting for your message"
 
-    begin
-      client_id = client.to_i
-      request = client.gets
-      puts "Request from ##{client_id}: #{request}"
-      client.puts "Hello"
-    ensure
-      client.close
+        begin
+          client_id = client.to_i
+          request = client.gets
+          puts "Request from ##{client_id}: #{request}"
+          client.puts "Hello"
+        ensure
+          client.close
+        end
+      end
     end
   end
+  servers.each(&:join)
 ensure
   socket.close
 end

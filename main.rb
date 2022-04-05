48c03ea6b3272cc355a226172a0bfccfeed99356
bajorekp <bajorekp@gmail.com>
2022-04-06 00:52:07 +0200

    Build TCP server

--- main.rb
+++ main.rb
@@ -1,1 +1,23 @@
-puts "Hi mum!"
+require 'socket'
+require 'pry'
+
+socket = TCPServer.new(4242)
+
+begin
+  puts "The server is running on port 4242..."
+  loop do
+    client = socket.accept
+    puts "Hi user! I'm waiting for your message"
+
+    begin
+      client_id = client.to_i
+      request = client.gets
+      puts "Request from ##{client_id}: #{request}"
+      client.puts "Hello"
+    ensure
+      client.close
+    end
+  end
+ensure
+  socket.close
+end
